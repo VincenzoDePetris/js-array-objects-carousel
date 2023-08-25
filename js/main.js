0dc11523881b24928f8c40321b912de8c17df520
VincenzoDePetris <vince02@live.it>
2023-08-25 17:29:37 +0200

    add: milestone 2

--- js/main.js
+++ js/main.js
@@ -1,70 +1,60 @@
 const images = [
   {
-    image: 'img/01.webp',
+    image: "img/01.webp",
     title: "Marvel's Spiderman Miles Morales",
-    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
   },
   {
-    image: 'img/02.webp',
-    title: 'Ratchet & Clank: Rift Apart',
-    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    image: "img/02.webp",
+    title: "Ratchet & Clank: Rift Apart",
+    text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
   },
   {
-    image: 'img/03.webp',
-    title: 'Fortnite',
-    text: 'Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.',
+    image: "img/03.webp",
+    title: "Fortnite",
+    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
   },
   {
-    image: 'img/04.webp',
-    title: 'Stray',
-    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    image: "img/04.webp",
+    title: "Stray",
+    text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
   },
   {
-    image: 'img/05.webp',
+    image: "img/05.webp",
     title: "Marvel's Avengers",
     text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
   },
 ];
-
 
 const main = document.getElementById("main");
 const prima = document.getElementById("prima");
 const dopo = document.getElementById("dopo");
 const slider = document.getElementById("slider");
 
+const items = document.getElementsByClassName("d-no");
 
+console.log(items);
 
+let index = 0;
 
+dopo.addEventListener("click", function () {
+  document.querySelector(".active").classList.remove("active");
+  if (index < 4) {
+    index++;
+  } else {
+    index = 0;
+  }
 
-for(game of images){
-  slider.innerHTML += `
-  <div class="card d-no" id="card">
-  <img src="./${game.image}" class="card-img-top" alt="${game.title}">
-  <div class="card-body">
-  <h3>${game.title}</h3>
-  <p class="card-text">${game.text}</p>
-  </div>
-  </div>
-  `
-}
+  items[index].classList.add("active");
+});
 
-function boh(){
-  game++;
-}
+prima.addEventListener("click", function () {
+  document.querySelector(".active").classList.remove("active");
+  if (index > 0) {
+    index--;
+  } else {
+    index = 4;
+  }
 
-a
-
-
-document.getElementById("card").classList.remove("d-no");
-
-
-
-
-
-
-
-
-
-
-
-
+  items[index].classList.add("active");
+});
